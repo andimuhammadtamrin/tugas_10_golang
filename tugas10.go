78fcfcd3e8bea84ef35b2e4295fede5fd8c7d598
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2020-05-18 17:04:20 +0800

    Tugas 10 Golang
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas10.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "time"
+
+func main()  {
+  fmt.Println("Mulai")
+  time.Sleep(time.Second * 2)
+  fmt.Println("Nama : Andi Muhammad Tamrin")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Hoby : Main Game")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Tanggal Lahir : 10-02-1994")
+  time.Sleep(time.Second * 1)
+  fmt.Println("Selesai")
+}
